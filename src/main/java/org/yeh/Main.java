431a30054d5340bc3bb7f6e78a3e0e0f5068d960
Yeh-Wang <wyx1627082750@163.com>
2023-01-10 21:39:22 +0800

    Yeh Wang

---
+++ src/main/java/org/yeh/Main.java
@@ -0,0 +1,11 @@
+package org.yeh;
+
+/**
+ * @author :Yaxin-Wang
+ * @date : 2023/1/10
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
